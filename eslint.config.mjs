419e1a615df512ffca0f35904b60ed8d8910de26
msaoandon <ms.aoandon@gmail.com>
2026-04-04 21:48:37 +0200

    init: eslint and prettier config

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.strict, prettierConfig, {
+  ignores: ["**/node_modules/", "**/dist/", "**/.next/", "**/coverage/"],
+});
