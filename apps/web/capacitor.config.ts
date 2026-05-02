bfad6d2c3e0a27fee1574e6f1d51a8e918c72886
msaoandon <ms.aoandon@gmail.com>
2026-05-02 19:59:36 +0200

    setup/capacitor-ios: Setup Capacitor for iOs

---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.msaoandon.hyakuto',
+  appName: 'Hyakuto',
+  webDir: 'out'
+};
+
+export default config;
